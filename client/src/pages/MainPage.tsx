6634368bc91134ee7090766674dbd7cf1e166571
Isiah Jordan Dimaunahan <dimaunahanisiahjordan@gmail.com>
2025-04-21 05:18:52 +0800

    feat: main function

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,12 +1,48 @@
 import React from "react";
 import Header from "../components/Header.tsx";
+import IntroSec from "../components/Intro.tsx";
+import {BrandService, BrandWork} from "../components/BrandDetails.tsx";
+import AboutSec from "../components/About.tsx";
+import Skills from "../components/Skills.tsx";
+import WorkList from "../components/Works.tsx";
+import {scrollToSection} from "../utilities/animation.ts";
+import {TabData} from "../utilities/types.ts";
+import ContactModal from "../components/Contacts.tsx";
+import {useState} from "react";
+import Certificates from "../components/Certificate.tsx";
+import Footer from "../components/Footer.tsx";
+
 
 const Main: React.FC = () => {
-  return (
+  const [isModalOpen, setModalOpen] = useState(false);
+  const tabs_data: TabData[] = [
+    { label: "home", action: () => scrollToSection("home")},
+    { label: "about", action: () => scrollToSection("about")},
+    { label: "skills", action: () => scrollToSection("skills")},
+    { label: "works", action: () => scrollToSection("works")},
+    { label: "contacts", action: () => setModalOpen(true)},
+  ];
+  return(
     <>
-      <Header/>
+      <Header
+        lastname="DIMAUNAHAN"
+        tabs= {tabs_data}
+      />
+      <ContactModal
+        isModalOpen={isModalOpen}
+        setModalOpen={setModalOpen}
+      />
+      <IntroSec/>
+      <BrandService/>
+      <AboutSec/>
+      <Skills/>
+      <BrandWork/>
+      <WorkList/>
+      <Certificates/>
+      <Footer/>
     </>
   )
 }
 
 export default Main;
+
